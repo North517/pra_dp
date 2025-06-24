7e6fdaf1de961a84a2c18663c0ab64affec21a68
north001 <north@north001.cn>
2025-06-24 23:59:56 +0800

    缓存击穿————逻辑过期时间解决

--- src/main/java/com/hmdp/utils/RedisData.java
+++ src/main/java/com/hmdp/utils/RedisData.java
@@ -5,7 +5,11 @@ import lombok.Data;
 import java.time.LocalDateTime;
 
 @Data
-public class RedisData {
+public class RedisData{
     private LocalDateTime expireTime;
+    /**
+     * 通过使用 Object 类型，可以将缓存层的实现与具体业务对象解耦。
+     * 缓存工具类可以存储任意类型的对象，方便操作
+     */
     private Object data;
 }
--- src/test/java/com/hmdp/HmDianPingApplicationTests.java
+++ src/test/java/com/hmdp/HmDianPingApplicationTests.java
@@ -1,9 +1,22 @@
 package com.hmdp;
 
+import com.hmdp.service.impl.ShopServiceImpl;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+
+import javax.annotation.Resource;
 
 @SpringBootTest
 class HmDianPingApplicationTests {
+    @Resource
+    private ShopServiceImpl shopService;
+
+    @Test
+    void testSaveShop() throws InterruptedException {
+        shopService.save2Redis(1L,10L);
+    }
+
 
 
 }
